58da5512ec0590fd5876788184cf140a523ccd89
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-04-03 18:12:50 +0530

    CameraIntent Demo Application @ 2022 #032AbhishekChoksi. All rights reserved.

---
+++ app/src/main/java/com/example/cameraintentdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.cameraintentdemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int pic_id = 123;
+    Button button;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent camera_intent =
+                        new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == pic_id) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}
